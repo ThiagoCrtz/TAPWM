8c5a9daae3dccb40b720030e6ffc4863417c8d22
Thiago Cortez <112135993+ThiagoCrtz@users.noreply.github.com>
2026-04-22 18:03:25 -0300

    Refactor verificarTriangulo function

--- Atividade6/script.js
+++ Atividade6/script.js
@@ -9,25 +9,20 @@ function verificarTriangulo() {
 
             let displayResultado = document.getElementById('resultado');
             
-            // Limpa as classes anteriores antes de uma nova verificação
             displayResultado.className = '';
 
-            // 1. Validação se os valores são números
             if (isNaN(a) || isNaN(b) || isNaN(c) || inputA.trim() === '' || inputB.trim() === '' || inputC.trim() === '') {
                 displayResultado.innerText = "Erro: Por favor, insira apenas números válidos.";
                 displayResultado.classList.add('erro');
                 return;
             }
 
-            // 2. Regra para pertencer aos lados de um triângulo
             let regra1 = Math.abs(b - c) < a && a < (b + c);
             let regra2 = Math.abs(a - c) < b && b < (a + c);
             let regra3 = Math.abs(a - b) < c && c < (a + b);
 
-            // Verifica se formam um triângulo
             if (regra1 && regra2 && regra3) {
                 
-                // 3. Verifica o tipo do triângulo
                 let tipo = "";
                 if (a === b && b === c) {
                     tipo = "Equilátero";
