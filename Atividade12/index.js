b39def02e541d00e2d2d37d7f5d074d199e8b583
Thiago Cortez <112135993+ThiagoCrtz@users.noreply.github.com>
2026-05-27 17:47:42 -0300

    Create index.js

---
+++ Atividade12/index.js
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Usando eventos no Javascript</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f0f0f0;
+        }
+        img {
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    
+    <img src="janelafechada.png" id="janela" 
+         onmouseout="fechar(this)" 
+         onmouseover="abrir(this)" 
+         onclick="quebrar(this)" 
+         alt="Janela">
+
+    <script>
+        let janelaQuebrada = false;
+
+        function abrir(obj) {
+            if (!janelaQuebrada) {
+                obj.src = "janelaberta.png";
+            }
+        }
+
+        function fechar(obj) {
+            if (!janelaQuebrada) {
+                obj.src = "janelafechada.png";
+            }
+        }
+
+        function quebrar(obj) {
+            obj.src = "janelaquebrada.jpg";
+            janelaQuebrada = true; 
+        }
+    </script>
+</body>
+</html>
